abafe40e0e85406b750ef346eb2c640fcd33f538
DDongu <57486026+DDongu@users.noreply.github.com>
2025-03-28 03:20:53 +0900

    Create: terminate_bot.sh 종료 실행 파일

---
+++ terminate_bot.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# music_bot.py 실행 중인 프로세스 찾기
+PID=$(ps aux | grep music_bot.py | grep -v grep | awk '{print $2}')
+
+if [[ -z "$PID" ]]; then
+  echo "❌ 실행 중인 봇 프로세스를 찾을 수 없어요."
+else
+  echo "🛑 봇 프로세스 종료 중... (PID: $PID)"
+  sudo kill -9 $PID
+  echo "✅ 봇이 성공적으로 종료되었습니다."
+fi
